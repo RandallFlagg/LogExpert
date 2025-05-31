d7a84fb7838e02108fa13975d5896f712de66505
Randall Flagg <O@G>
2025-05-31 23:38:19 +0300

    Removed UI dependencies from DefaultPlugins by removing relevant code to the UI project. changed other projects to be .net8 and not .net8-windows.

---
+++ src/LogExpert.UI/Dialogs/Eminus/EminusConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LogExpert
+{
+    [Serializable]
+    internal class EminusConfig
+    {
+        #region Fields
+
+        public string host = "127.0.0.1";
+        public string password = "";
+        public int port = 12345;
+
+        #endregion
+
+        #region Public methods
+
+        public EminusConfig Clone()
+        {
+            EminusConfig config = new()
+            {
+                host = host,
+                port = port,
+                password = password
+            };
+            return config;
+        }
+
+        #endregion
+    }
+}
