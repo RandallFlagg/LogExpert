6b3d97d2f56cfaf8f5f069c1304ecee562e306fe
Hirogen <hirogenat@gmail.com>
2025-06-16 09:10:18 +0200

    removing more darkmode stuff

--- src/LogExpert.UI/Extensions/Forms/MenuStripExtension.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.Versioning;
-
-namespace LogExpert.UI.Extensions.Forms;
-
-[SupportedOSPlatform("windows")]
-internal class ExtendedMenuStripRenderer : ToolStripProfessionalRenderer
-{
-    public ExtendedMenuStripRenderer() : base(new MenuSelectedColors()) { }
-}
